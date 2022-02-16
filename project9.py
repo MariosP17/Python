5de6576cf3c4b33b03f1a848bd5bce5496b451c1
MariosP17 <94470234+MariosP17@users.noreply.github.com>
2022-02-16 04:27:57 +0200

    Update project9.py

--- project9.py
+++ project9.py
@@ -1,4 +1,5 @@
 import math
+from re import I
 def toBinary(a):
   l,m=[],[]
   for i in a:
@@ -13,37 +14,37 @@ def changeToBin(a):
     with open(a, 'w+') as result:
          result.writelines(Bin[i]+' ' for i in range(len(Bin)))
     f.close()
-def readBin(a):
+def findSeq(a):
     f = open(a, 'r+')
     read = f.read()
     new = read.split(' ')
     new.pop()
-    if any("11" in s for s in new):
+    if any("11" in i for i in new):
      num = 2
-    if any("111" in s for s in new):
+    if any("111" in i for i in new):
      num = 3 
-    if any("1111" in s for s in new):
+    if any("1111" in i for i in new):
      num = 4
-    if any("11111" in s for s in new):
+    if any("11111" in i for i in new):
      num = 5
-    if any("111111" in s for s in new):
+    if any("111111" in i for i in new):
      num = 6
-    if any("1111111" in s for s in new):
+    if any("1111111" in i for i in new):
      num = 7
-    if any("00" in s for s in new):
+    if any("00" in i for i in new):
      num0 = 2
-    if any("000" in s for s in new):
+    if any("000" in i for i in new):
      num0 = 3 
-    if any("0000" in s for s in new):
+    if any("0000" in i for i in new):
      num0 = 4
-    if any("00000" in s for s in new):
+    if any("00000" in i for i in new):
      num0 = 5
-    if any("000000" in s for s in new):
+    if any("000000" in i for i in new):
      num0 = 6
-    if any("0000000" in s for s in new):
+    if any("0000000" in i for i in new):
      num0 = 7
     print("Η μεγαλύτερη ακολουθία από 1 είναι " + str(num) + " άσσοι!")
     print("Η μεγαλύτερη ακολουθία από 0 είναι " + str(num0) + " μηδενικά!")
     f.close()
 changeToBin('text.txt')
-readBin("text.txt")
+findSeq("text.txt")
